7e32fb0f39cfc6436c2f610f7fefa5caed49e185
David Mitchell <monch1962@gmail.com>
2021-02-28 06:36:24 +0000

    WIP

---
+++ karate-config/karate-config.js
@@ -0,0 +1,12 @@
+function() {
+    var config = {
+        baseUrl: 'http://localhost:8888',
+        proxy: {
+            uri: 'http://localhost:18500'
+        }
+    }
+    karate.configure('proxy', config.proxy);
+    karate.configure('readTimeout', 10000);
+    karate.configure('connectTimeout', 10001);
+    return config;
+}
